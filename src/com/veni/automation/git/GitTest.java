62d83688ad58fae7104b80860d7dfafa3e050f78
venitattari <akvsrm23@gmail.com>
2021-08-27 07:03:59 -0400

    added new line

--- src/com/veni/automation/git/GitTest.java
+++ src/com/veni/automation/git/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("hi, how are you");
                 System.out.println("hello, i am veni");
+                System.out.println("learning new course");  
 
 
 	}
