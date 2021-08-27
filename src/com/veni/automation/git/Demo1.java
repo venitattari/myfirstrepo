3c95736395d96a810ad7fed8a8946f61be598202
venitattari <akvsrm23@gmail.com>
2021-08-27 06:07:34 -0400

    changes in one class

--- src/com/veni/automation/git/Demo1.java
+++ src/com/veni/automation/git/Demo1.java
@@ -1,5 +1,10 @@
 package com.veni.automation.git;
 
 public class Demo1 {
+public static void main(String[] args) {
+	System.out.println("i will meet you");
+	System.out.println("tomorrow");
+	System.out.println("hello");
+}
 
 }
--- src/com/veni/automation/git/GitTest.java
+++ src/com/veni/automation/git/GitTest.java
@@ -3,7 +3,7 @@ package com.veni.automation.git;
 public class GitTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("hi, how are you");
 
 	}
 
