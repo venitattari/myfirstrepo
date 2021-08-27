fc97e5368d58ccf622f74fcda9f1b5a7affccb71
venitattari <akvsrm23@gmail.com>
2021-08-27 07:06:41 -0400

    person1 changes

--- src/com/veni/automation/git/GitTest.java
+++ src/com/veni/automation/git/GitTest.java
@@ -4,7 +4,8 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("hi, how are you");
-                System.out.println("hello, i am veni");
+        System.out.println("hello, i am veni");
+        System.out.println("java with selenium");
 
 
 	}
